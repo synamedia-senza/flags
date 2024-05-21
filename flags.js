c63f0bf8949bfba9ade74549fcf4e560812e0c33
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2024-05-21 10:18:47 -0400

    Now using deviceInfo.countryCode.

--- flags.js
+++ flags.js
@@ -1,9 +1,12 @@
 window.addEventListener("load", async () => {
   await hs.init();
-  let ipAddress = hs.deviceManager.deviceInfo.clientIp;
-  let ipdata = await (await fetch(`https://api.ipdata.co/${ipAddress}?api-key=${API_KEY}`)).json();
-  let countryCode = ipdata.country_code.toLowerCase();
-  image.src = `https://flagpedia.net/data/flags/w1160/${countryCode}.webp`;
-  console.log(ipAddress);
+
+  // let ipAddress = hs.deviceManager.deviceInfo.clientIp;
+  // let ipdata = await (await fetch(`https://api.ipdata.co/${ipAddress}?api-key=${API_KEY}`)).json();
+  // let countryCode = ipdata.country_code;
+  // console.log(ipAddress);
+
+  let countryCode = hs.deviceManager.deviceInfo.countryCode;
+  image.src = `https://flagpedia.net/data/flags/w1160/${countryCode.toLowerCase()}.webp`;
   console.log(countryCode);
 });
